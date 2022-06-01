7228fd8898c3c30a4c451e21c41e98f3cb18e8f8
fange123 <2030564240@qq.com>
2022-06-01 16:50:10 +0800

    feat:黑板添加清屏功能和设置背景色功能

--- blackboard/src/main.ts
+++ blackboard/src/main.ts
@@ -7,15 +7,21 @@ class BlackBoard {
   constructor(private el = document.querySelector<HTMLCanvasElement>('#canvas')!,
   private app = el.getContext('2d')!,
   private width:number = el.width,
-  private height:number = el.height){
+  private height:number = el.height,
+  private btnDiv:HTMLDivElement = document.createElement('div'),
+  private bgColor='#000',
+  private lineColor='#fff',
+  ){
     this.initCanvas()
     this.bindEvent()
 
   }
 
 private initCanvas(){
-  this.app.fillStyle = "#000"
+  this.app.fillStyle = this.bgColor
   this.app.fillRect(0,0,this.width,this.height)
+  this.btnDiv.style.cssText='margin-top:10px'
+  this.el.insertAdjacentElement('afterend',this.btnDiv)
 
 }
 
@@ -24,7 +30,7 @@ private bindEvent(){
   //* 鼠标落下开始画线
   this.el.addEventListener('mousedown',()=> {
     this.app.beginPath()
-    this.app.strokeStyle = '#fff'
+    this.app.strokeStyle = this.lineColor
     this.el.addEventListener('mousemove', callback)
   })
 
@@ -40,6 +46,29 @@ private drawLine(event:MouseEvent){
   this.app.lineTo(event.offsetX, event.offsetY)//#定义点
   this.app.stroke()//#把点连接起来
 
+}
+
+//* 设置背景颜色的操作
+public setBgColor(color:string){
+  this.bgColor = color
+  this.app.fillStyle = color;
+  this.app.fillRect(0,0,this.width,this.height)
+   return this
+}
+
+//* 清屏操作
+public clear() {
+  const el = document.createElement('button')
+  el.innerText = '清屏'
+  this.btnDiv.insertAdjacentElement('afterbegin',el)
+  el.addEventListener('click', ()=>{
+    this.app.fillStyle=this.bgColor
+    this.app.fillRect(0,0,this.width,this.height)
+  })
+  //* 返回this可以进行链式操作
+  return this
+
+
 
 }
 
@@ -47,3 +76,4 @@ private drawLine(event:MouseEvent){
 
 
 const instance = new BlackBoard()
+instance.clear().setBgColor('green')
