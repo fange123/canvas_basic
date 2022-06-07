9f1fe3ef88b90d2ea11454dfee30ec565ccd88af
fange123 <2030564240@qq.com>
2022-06-07 18:25:15 +0800

    feat:模型贴图

--- tank-project/src/canvas/canvas.ts
+++ tank-project/src/canvas/canvas.ts
@@ -1,21 +1,44 @@
 import config from '../config'
+import imgUrl from  '../static/images/straw/straw.png'
 // * 创建父级的类
 
 export default class CanvasAbstract{
+  //* items是用来在不同实例中记录不同对象的数据：比如记录坦克的数量，草地的数量，子弹的轨迹等等
+  protected items = []
   constructor(
     protected app = document.querySelector('#app') as HTMLDivElement,
     protected el:HTMLCanvasElement = document.createElement('canvas'),
     protected canvas = el.getContext('2d')!
   ){
+    this.createCanvas();
+    this.drawModels()
+  }
 
-    // # constructor参数使用了protected（或public等），外部使用可以不用写this
-    el.width = config.canvas.width
-    el.height = config.canvas.height
+  //* 绘制canvas
+  protected createCanvas(){
+   this. el.width = config.canvas.width
+    this.el.height = config.canvas.height
+    this.app.insertAdjacentElement('afterbegin',this.el)
+  }
 
-    canvas.fillStyle = 'yellow'
-    canvas.fillRect(0,0,400,400)
-    app.insertAdjacentElement('afterbegin',el)
+  //* 画对象模型
+  protected drawModels(){
+    const img = document.createElement('img')
+    img.src = imgUrl
+    const position  = this.position() ;
+    //# 图片加载是异步的，需要加载完再绘制
+        img.addEventListener('load', () => {
+          this.canvas.drawImage(img, position.x,position.y,config.model.width,config.model.height);
+        });
 
+  }
+
+  //* 模型的随机位置的计算
+  protected position(){
+    return {
+      x:40,
+      y:80
+    }
   }
 
 }
--- tank-project/src/config.ts
+++ tank-project/src/config.ts
@@ -1,6 +1,15 @@
+//* 基本配置
 export default {
+  // * 画布的基本配置
   canvas:{
     width:900,
     height:600
+  },
+
+  // * 模型的基本配置
+  model:{
+    width:30,
+    height:30
+
   }
 }
