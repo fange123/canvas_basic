3ea6ed9de110c9eb418a627cf1f5d53276e3b68e
fange123 <2030564240@qq.com>
2022-06-08 15:41:41 +0800

    feat:批量生成模型随机坐标

--- tank-project/src/canvas/canvas.ts
+++ tank-project/src/canvas/canvas.ts
@@ -24,16 +24,25 @@ export default abstract class CanvasAbstract{
 
   //* 画对象模型
   protected drawModels(n:number){
-    const position  = this.position();
     //渲染多个草坪
-    Array(n).fill('').forEach(()=> this.canvas.drawImage(image.get('straw')!, position.x,position.y,config.model.width,config.model.height))
+    this.positionCollection(n).forEach((position)=>this.canvas.drawImage(image.get('straw')!, position.x,position.y,config.model.width,config.model.height))
+  }
+
+  //* 批量随机生成,但是随机生成坐标可能有重复的，会导致贴图重叠
+  protected positionCollection(n:number){
+    const collection = []
+    for (let index = 0; index < n; index++) {
+      collection.push(this.position())
+    }
+    return collection
+
   }
 
   //* 模型的随机位置的计算
   protected position(){
     return {
-      x:40,
-      y:80
+      x:Math.floor(Math.random()*(config.canvas.width/config.model.width))*config.model.width,
+      y:Math.floor(Math.random()*(config.canvas.height/config.model.height))*config.model.height,
     }
   }
 
