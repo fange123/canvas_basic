3aeb9ac72f8acbff48fc1812331451451819d260
fange123 <2030564240@qq.com>
2022-06-10 15:14:26 +0800

    feat:让坦克动起来

--- tank-project/src/model/tank.ts
+++ tank-project/src/model/tank.ts
@@ -11,7 +11,40 @@ export default class extends ModelAbstract implements IModel{
   render(): void {
     this.randomPosition()
     super.draw(this.renderImage())
+
+    //# 循环定时器让坦克动起来
+    setInterval(()=> {
+      this.move()
+
+    },50)
   }
+
+protected move(){
+  //* 每次运动前需要清理一下画布，如果只清理坦克，坦克数量多的时候影响性能
+  this.canvas.clearRect(this.x,this.y,config.model.width,config.model.height)
+  switch (this.direction) {
+    case directEnum.top:
+      this.y+=2
+      break;
+    case directEnum.right:
+      this.x+=2
+      break;
+    case directEnum.bottom:
+      this.y-=2
+      break;
+    case directEnum.left:
+      this.x-=2
+      break;
+
+    default:
+      break;
+  }
+  //* 改变完方向再把坦克重绘一下
+  super.draw(this.renderImage())
+
+
+}
+
 //* 绘制坦克的图片要随机生成
 renderImage(){
   const direction = this.name + _.upperFirst(this.direction)
