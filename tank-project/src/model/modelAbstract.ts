d92ec75e24f14513c743fa4bcbfb041e8cea0eec
fange123 <2030564240@qq.com>
2022-06-10 14:53:57 +0800

    feat:通过lodash改变方向

--- tank-project/src/model/modelAbstract.ts
+++ tank-project/src/model/modelAbstract.ts
@@ -3,6 +3,7 @@ import config from "../config";
 //*定义模型的抽象父类，方便子类继承
 export abstract class  ModelAbstract{
   abstract render():void
+  abstract name:string
   constructor(
     protected canvas:CanvasRenderingContext2D,protected x:number, protected y:number){
 
--- tank-project/src/model/tank.ts
+++ tank-project/src/model/tank.ts
@@ -1,17 +1,22 @@
 import { image } from "../service/image";
 import { ModelAbstract } from "./modelAbstract";
 import { directEnum } from "../enum/positionEnum";
+import _ from 'lodash'
+import config from "../config";
 
 export default class extends ModelAbstract implements IModel{
+  name: string = 'tank';
 
   protected direction:directEnum = directEnum.top
   render(): void {
+    this.randomPosition()
     super.draw(this.renderImage())
   }
 //* 绘制坦克的图片要随机生成
 renderImage(){
-  const direction = this.randomPosition()
-  return image.get('tankTop')!
+  const direction = this.name + _.upperFirst(this.direction)
+  //~lodash库的作用就是为了把首字母转成大写
+  return image.get(direction as keyof typeof config.images)!
 
 }
 
